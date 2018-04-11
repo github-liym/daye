20eb4b7f9d80807b2ec05a7631db58e1ec2ecb3c
unknown <>
2018-04-11 11:39:16 +0800

    备份

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -4,4 +4,22 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import scss from '../css/main.scss'
 import 'bootstrap'
+import Swiper from 'swiper'
+/*
+var mySwiper = new Swiper('.swiper-container', {
+	direction: 'vertical',
+	autoplay: true,
+	loop: true
+})*/
 
+var swiper = new Swiper('.swiper-container', {
+	direction: 'vertical',
+	slidesPerView: 1,
+	autoplay: {
+		delay: 4000,
+		stopOnLastSlide: false,
+		disableOnInteraction: true,
+	},
+	spaceBetween: 20,
+	loop: true,
+});
